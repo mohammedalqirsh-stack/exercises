db581b5bb2fabca261482c3705543e26eeae52bc
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-01-04 12:02:58 +0200

    add new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/quiz.dart';
+import 'package:flutter_course/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-    body: )));
+  runApp(const Quiz());
 }
-
-   
---
+++ lib/questins_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestinsScreen extends StatefulWidget {
+  const QuestinsScreen({super.key});
+
+  @override
+  State<QuestinsScreen> createState() => _QuestinsScreenState();
+}
+
+class _QuestinsScreenState extends State<QuestinsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('QuestionsScreen');
+  }
+} 
