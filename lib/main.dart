f8578ee21352f73f66e8d92abb08dbe47e703ca2
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-01-04 09:17:18 +0200

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(MaterialApp(
+    home: Scaffold(
+    body: GradientContainer())));
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 45, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Mohammed hani",
-          style: TextStyle(color: Colors.white, fontSize: 30),
-        ),
-      ),
-    );
-  }
-}
+  
